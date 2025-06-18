022e23f1d76d7069078682eec68cb6fa3a75f409
arjunrathod1996 <arjundargurathod@gmail.com>
2025-06-19 00:31:17 +0530

    Updated

--- src/test/java/com/SCM/ScmApplicationTests.java
+++ src/test/java/com/SCM/ScmApplicationTests.java
@@ -1,12 +1,17 @@
 package com.SCM;
 
+import com.SCM.role.RoleInitializer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")  // This will now use your updated application-test.properties
+@ActiveProfiles("test")
+@TestConfiguration
+@ImportAutoConfiguration(exclude = RoleInitializer.class)
 class ScmApplicationTests {
 	@Test
 	@Disabled("Disabling temporarily for CI")
