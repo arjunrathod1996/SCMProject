6f59eba2421c319147f62242d0d4378081a2efb5
arjunrathod1996 <arjundargurathod@gmail.com>
2025-06-18 23:43:35 +0530

    Updated pom.xml and ScmApplicationTests File

--- src/test/java/com/SCM/ScmApplicationTests.java
+++ src/test/java/com/SCM/ScmApplicationTests.java
@@ -3,14 +3,10 @@ package com.SCM;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.hibernate.ddl-auto=none"
-})
+@SpringBootTest
+@ActiveProfiles("test")
 class ScmApplicationTests {
 
 	@Test
