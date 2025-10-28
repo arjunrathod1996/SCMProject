30867d3460d4cc0bf7a1af0fec0c9787d9ec1c6b
arjunrathod1996 <arjundargurathod@gmail.com>
2025-10-28 17:36:05 +0530

    updated maven.yml

--- src/main/java/com/SCM/entities/User.java
+++ src/main/java/com/SCM/entities/User.java
@@ -56,9 +56,9 @@ public class User extends BigBaseEntity { // implements UserDetails
     @Column(name = "user_id")
     private String userId;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name", nullable = true)
     private String firstName;
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name", nullable = true)
     private String lastName;
 
     @Column(unique = true) //, nullable = true
